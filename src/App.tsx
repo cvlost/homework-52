8f71632f27dd441ee1fd2701fb22d0b6d3a5338c
Pavlov Vladimir <connection.lost.again@gmail.com>
2022-10-24 07:18:03 +0600

    Added a button and state for displaying cards

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,32 @@
-import React from 'react';
+import React, {useRef, useState} from 'react';
 import './App.css';
 import './cards.css';
 import CardView from "./components/CardView/CardView";
+import Card from "./lib/Card";
+import CardDeck from "./lib/CardDeck";
 
 function App() {
+  const cardDeck = useRef(new CardDeck());
+  const [cards, setCards] = useState<Card[]>([]);
+
+  const getCards = () => {
+    const cards = cardDeck.current.getCards(5);
+    setCards(cards);
+  };
+
+  const button = <button onClick={getCards}>Cards</button>
+
+  if (cards.length === 0) return button;
+
   return (
     <div className="App faceImages playingCards">
-      <CardView suit={'clubs'} rank={'A'} />
+      {cards.map((card,i) =>
+        <CardView
+          key={i}
+          suit={card.suit} rank={card.rank}
+        />
+      )}
+      <div>{button}</div>
     </div>
   );
 }
